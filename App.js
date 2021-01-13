aa10068f473aafde9823586121a68f7414def3d3
Christopher Owen <c.ornine@gmail.com>
2021-01-13 18:14:14 +0000

    feat: print phones current location to screen

--- App.js
+++ App.js
@@ -6,12 +6,32 @@ import * as Location from 'expo-location';
 
 export default function App() {
   const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS === 'android' && !Constants.isDevice) {
+        setErrorMsg(
+          'Sorry, this will not work in an Android emulator. Try it on your device!'
+        );
+        return;
+      }
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getLastKnownPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
 
   return (
     <View style={styles.container}>
       <Text>Wandrer (proto)</Text>
       <Text style={styles.subtitle}>made using Freesound</Text>
-      <Text>CHANGE</Text>
+      <Text>{JSON.stringify(location)}</Text>
       <StatusBar style="auto" />
     </View>
   );
