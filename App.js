dffe6b75ecb208875962f5991555b9a95d861644
Christopher Owen <c.ornine@gmail.com>
2021-01-13 18:35:09 +0000

    feat: add error message and loading feedback to location fetch

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import Constants from 'expo-constants';
 import * as Location from 'expo-location';
@@ -10,28 +10,38 @@ export default function App() {
 
   useEffect(() => {
     (async () => {
+      //* check whether app is running on phone or emulator
       if (Platform.OS === 'android' && !Constants.isDevice) {
         setErrorMsg(
-          'Sorry, this will not work in an Android emulator. Try it on your device!'
+          'Sorry, this will not work in an Android emulator. Try it on your device!',
         );
         return;
       }
+      //* set error message and return if permission denied
       let { status } = await Location.requestPermissionsAsync();
       if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
+        setErrorMsg('Sorry, permission to access location was denied');
         return;
       }
-
+      //* fetch location
       let location = await Location.getLastKnownPositionAsync({});
       setLocation(location);
     })();
   }, []);
 
+  let locationText = 'Fetching location...';
+  if (errorMsg) {
+    locationText = errorMsg;
+  } else if (location) {
+    let { longitude, latitude } = location.coords;
+    locationText = 'You are at ' + longitude + ',' + latitude;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Wandrer (proto)</Text>
       <Text style={styles.subtitle}>made using Freesound</Text>
-      <Text>{JSON.stringify(location)}</Text>
+      <Text>{locationText}</Text>
       <StatusBar style="auto" />
     </View>
   );
